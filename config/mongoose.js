d23f2ee12a7f0939ff293b665b0389a99046a234
ParasPandey019 <paraspandey019@gmail.com>
2023-10-13 14:40:58 +0530

    mongoose config added

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/issueTracker');
+
+const db = mongoose.connection;
+
+db.on('error',console.error.bind(console, 'error connecting to db'));
+db.once('open', function(){
+    console.log('successfully connected to databse');
+})
+
+module.exports = db;
