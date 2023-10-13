1dff07fa0f4856fdbbfa38646c33e5c87238a55f
ParasPandey019 <paraspandey019@gmail.com>
2023-10-13 14:10:58 +0530

    basic routes added as in problem description

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+
+// Route for home page
+router.route("/");
+
+// Route to create project page and create project
+router.route("/create_project");
+
+// Route to project details page
+router.route("/project_details/:id");
+
+// Route to create issue page and create issue
+router.route("/create_issue/:id");
+
+// Exporting all the routes
+module.exports = router;
+
