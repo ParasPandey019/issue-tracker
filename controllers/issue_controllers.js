8ac953439635071da227248924da57783eb22fe3
ParasPandey019 <paraspandey019@gmail.com>
2023-10-15 13:07:42 +0530

    issue controllers and css files added

--- controllers/issue_controllers.js
+++ controllers/issue_controllers.js
@@ -1,7 +1,19 @@
-module.exports.page = function(req,res){
-
+module.exports.page =async function(req,res){
+    const project = await ProjectModel.findById(req.params.id);
+    res.render('createIssue',{title:"Create Issue ", project});
 }
 
-module.exports.create = function(req,res){
-    
+module.exports.create = async function(req,res){
+    const project = await ProjectModel.findById(req.params.id);
+    const issue = await IssueModel.create({
+        title:req.body.title,
+        description:req.body.description,
+        label:req.body.label,
+        issueAuthor:req.body.author
+    });
+    // adding the issue in project model
+    project.issues.push(issue);
+    await project.save();
+    await issue.save();
+    res.redirect("back");
 }
